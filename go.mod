638aadc4a7f64f2624dbb78543afe09f3e6eda05
Jon Inge Moe <jim@sol.no>
2020-08-24 08:49:25 +0200

    Updated packages

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/dbmedialab/harvester-protobufs
 go 1.12
 
 require (
-	github.com/golang/protobuf v1.3.2
-	google.golang.org/grpc v1.23.0
+	github.com/golang/protobuf v1.4.2
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200821140526-fda516888d29 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200815001618-f69a88009b70 // indirect
+	google.golang.org/grpc v1.31.0
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- harvesterinternalapi.pb.go
+++ harvesterinternalapi.pb.go
@@ -1694,11 +1694,11 @@ var fileDescriptor_956f1945bd8023db = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // HarvesterInternalApiClient is the client API for HarvesterInternalApi service.
 //
@@ -1726,10 +1726,10 @@ type HarvesterInternalApiClient interface {
 }
 
 type harvesterInternalApiClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewHarvesterInternalApiClient(cc *grpc.ClientConn) HarvesterInternalApiClient {
+func NewHarvesterInternalApiClient(cc grpc.ClientConnInterface) HarvesterInternalApiClient {
 	return &harvesterInternalApiClient{cc}
 }
 
